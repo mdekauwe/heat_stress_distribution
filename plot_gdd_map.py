a0e7205dbdadd3dffa9f51254fcb2bce90afcee2
mdekauwe <mdekauwe@gmail.com>
2018-04-24 14:52:17 +1000

    Added functionality to plot

--- plot_gdd_map.py
+++ plot_gdd_map.py
@@ -21,14 +21,46 @@ def main(input_dir):
     nrows = 67
     ncols = 83
 
-    gdd = np.fromfile(os.path.join(input_dir, "gdd.bin").reshape(nrows, ncols)
-    map = np.fromfile(os.path.join(input_dir, "map.bin").reshape(nrows, ncols)
+    fname = os.path.join(input_dir, "gdd.bin")
+    gdd = np.fromfile(fname, dtype=np.float64).reshape(nrows, ncols)
+    fname = os.path.join(input_dir, "map.bin")
+    map = np.fromfile(fname, dtype=np.float64).reshape(nrows, ncols)
 
-    gdd = np.where(gdd < -9000.0, np.nan, gdd)
-    map = np.where(map < -9000.0, np.nan, map)
+    gdd = np.where(gdd < -900.0, np.nan, gdd)
+    map = np.where(map < -900.0, np.nan, map)
 
-    plt.imshow(gdd)
-    plt.colorbar()
+    width = 14
+    height = 5
+    fig = plt.figure(figsize=(width, height))
+    fig.subplots_adjust(hspace=0.1)
+    fig.subplots_adjust(wspace=0.3)
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['font.family'] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = "Helvetica"
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['legend.fontsize'] = 10
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    ax1.set_title("GDD")
+    im1 = ax1.imshow(gdd, interpolation='None')
+    divider = make_axes_locatable(ax1)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    fig.colorbar(im1, cax=cax, orientation='vertical')
+
+    ax2.set_title("MAP")
+    im2 = ax2.imshow(map, interpolation='None')
+    divider = make_axes_locatable(ax2)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    fig.colorbar(im2, cax=cax, orientation='vertical');
+
+
     plt.show()
 
 
